630601c241d7c2a2ccd820a7c2eeea20c588a068
PreciousNwogu <nwoguprecious93@gmail.com>
2025-02-04 19:52:47 -0500

    complete task 3

--- data.php
+++ data.php
@@ -1,32 +1,43 @@
 <?php
-session_start();
 
-$items = [
-  [
-    'id' => 1,
-    'task' => 'Buy Milk',
-    'completed' => false,
-    'priority' => 1
-  ],
-  [
-    'id' => 2,
-    'task' => 'Feed Cat',
-    'completed' => true,
-    'priority' => 3
-  ],
-  [
-    'id' => 3,
-    'task' => 'Clean Room',
-    'completed' => false,
-    'priority' => 0
-  ]
-];
+$dsn='mysql:host=localhost; dbname=tasks';
+$username='root';
+$password='CST8250!';
 
-if (isset($_SESSION['items'])) {
-  $items = $_SESSION['items'];
-} else {
-  $_SESSION['items'] = $items;
+try{
+  $db =new PDO ($dsn, $username, $password);
+}catch(PDOException $e){
+  echo $e->getMessage();
+
 }
+// session_start();
+
+// $items = [
+//   [
+//     'id' => 1,
+//     'task' => 'Buy Milk',
+//     'completed' => false,
+//     'priority' => 1
+//   ],
+//   [
+//     'id' => 2,
+//     'task' => 'Feed Cat',
+//     'completed' => true,
+//     'priority' => 3
+//   ],
+//   [
+//     'id' => 3,
+//     'task' => 'Clean Room',
+//     'completed' => false,
+//     'priority' => 0
+//   ]
+// ];
+
+// if (isset($_SESSION['items'])) {
+//   $items = $_SESSION['items'];
+// } else {
+//   $_SESSION['items'] = $items;
+// }
 
 $priorities = [
   'None',
